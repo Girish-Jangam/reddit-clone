6dfe76c17fb42d2c904ef03421f1a9ee2878a59a
girish02 <[girishajangam@gmail.com]>
2023-02-03 21:20:41 -0500

    Navbar with authentication created

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+import { auth } from "@/src/firebase/clientApp";
+import { Flex, Image } from "@chakra-ui/react";
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import RightContent from "./RightContent/RightContent";
+import SearchInput from "./SearchInput";
+
+const Navbar: React.FC = () => {
+  const [user, loading, error] = useAuthState(auth);
+  return (
+    <Flex bg="white" height="44px" padding="6px 12px">
+      <Flex align="center">
+        <Image src="/images/redditFace.svg" height="30px" />
+        <Image
+          src="/images/redditText.svg"
+          height="40px"
+          display={{ base: "none", md: "unset" }}
+        />
+      </Flex>
+      {/* <Directory /> */}
+      <SearchInput />
+      <RightContent user={user} />
+    </Flex>
+  );
+};
+export default Navbar;
